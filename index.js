014b8b78d61a6546c9f690ddcbd9a5b22ac8bce0
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2019-06-17 22:13:12 +0100

    Refactor function to use onChange
    Passes an ENUM object of  to a single  function in order to provide the same functionality as before, but with a much more digestible API

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-module.exports = (config = {}) => {
-  const {
-    activateDarkMode = () => {},
-    activateLightMode = () => {},
-    activateFallback = () => {}
-  } = config;
-  const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  const lightQuery = window.matchMedia('(prefers-color-scheme: light)');
-  const noPrefQuery = window.matchMedia('(prefers-color-scheme: no-preference)');
+module.exports = ({ onChange }) => {
+  const themes = Object.freeze({
+    DARK: 'dark',
+    LIGHT: 'light',
+    NO_PREF: 'no-preference',
+    NO_SUPP: 'no-support'
+  });
+  const darkQuery = window.matchMedia(`(prefers-color-scheme: ${themes.DARK})`);
+  const lightQuery = window.matchMedia(`(prefers-color-scheme: ${themes.LIGHT})`);
+  const noPrefQuery = window.matchMedia(`(prefers-color-scheme: ${themes.NO_PREF})`);
   const hasNoSupport = !darkQuery.matches && !lightQuery.matches && !noPrefQuery.matches;
 
-  if (darkQuery.matches) activateDarkMode();
-  if (lightQuery.matches) activateLightMode();
-  if (noPrefQuery.matches || hasNoSupport) activateFallback();
+  if (hasNoSupport) {
+    return onChange(themes.NO_SUPP, themes);
+  }
 
-  darkQuery.addListener(e => e.matches && activateDarkMode());
-  lightQuery.addListener(e => e.matches && activateLightMode());
+  if (darkQuery.matches) onChange(themes.DARK, themes);
+  if (lightQuery.matches) onChange(themes.LIGHT, themes);
+  if (noPrefQuery.matches) onChange(themes.NO_PREF, themes);
+
+  darkQuery.addListener(e => e.matches && onChange(themes.DARK, themes));
+  lightQuery.addListener(e => e.matches && onChange(themes.LIGHT, themes));
 };
